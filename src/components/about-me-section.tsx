8e8c6c3ccd3f53ef9c1f65e260bc2b02dc2d2cd4
gustavocadev <gustavocadev@gmail.com>
2024-12-01 13:06:44 -0500

    Adjust padding and text sizes in About Me section for improved layout and readability

--- src/components/about-me-section.tsx
+++ src/components/about-me-section.tsx
@@ -2,7 +2,7 @@ import { component$ } from '@builder.io/qwik';
 
 export default component$(() => {
   return (
-    <section class="grid grid-cols-1 gap-y-[38px] xl:grid-cols-7 px-[120px] py-[96px] gap-x-[120px]">
+    <section class="grid grid-cols-1 gap-y-[38px] xl:grid-cols-7 px-6 lg:px-[120px] py-[96px] gap-x-[120px]">
       <section class="col-span-1 mx-auto flex items-center xl:col-span-3">
         <figure class="w-full transition duration-200">
           <img
@@ -14,13 +14,13 @@ export default component$(() => {
           />
         </figure>
       </section>
-      <section class="col-span-1 mx-auto text-center lg:text-left xl:col-span-4 space-y-4">
-        <h2 class="text-4xl  font-semibold leading-snug">
+      <section class="col-span-1 mx-auto text-left xl:col-span-4 space-y-4">
+        <h2 class="text-2xl lg:text-4xl  font-semibold leading-snug">
           Hola, soy Gustavo. Me gusta ayudar a las personas a traves del
           software.
         </h2>
 
-        <h3 class="text-4xl text-gray-600 leading-snug">
+        <h3 class="text-2xl lg:text-4xl text-gray-600 leading-snug">
           Soy un un gran apasiado a los deportes extremos. No salgo mucho con mi
           familia, suelo estar realizando deportes como el breakdance cuando
           estoy aburrido.
