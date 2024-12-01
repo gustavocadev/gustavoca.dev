80df1b8d37b87057a2529e317073721f42ef3972
gustavocadev <gustavocadev@gmail.com>
2024-12-01 13:06:35 -0500

    Remove redundant text color class from body for improved styling consistency

--- src/root.tsx
+++ src/root.tsx
@@ -31,7 +31,7 @@ export default component$(() => {
       </head>
       <body
         lang="en"
-        class="text-dark w-full overflow-x-hidden bg-[#F7F6F3] dark:bg-[#181818] dark:text-gray-200"
+        class="w-full overflow-x-hidden bg-[#F7F6F3] dark:bg-[#181818]"
       >
         <RouterOutlet />
         {!isDev && <ServiceWorkerRegister />}
